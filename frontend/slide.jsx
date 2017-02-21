5ff39ec3045c2ebf83de235f24c85cd7dc1f8801
Jonathon Lin <jonathonlin93@yahoo.com>
2017-02-20 17:00:53 -0800

    add pictures for slides

---
+++ frontend/slide.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Slide extends React.Component {
+  render () {
+    return(
+      <div className="content display-container">
+        <img className="my-slides" src="https://upload.wikimedia.org/wikipedia/en/b/b7/Seven_Springs_of_A_Pink_Cover.jpg" alt="Seven Springs of Apink album cover"/>
+        <img className="my-slides" src="https://upload.wikimedia.org/wikipedia/en/5/59/SnowPink.jpg" alt="Snow Pink album cover"/>
+        <img className="my-slides" src="https://kpopcolorcodedlyrics.files.wordpress.com/2012/05/une-annee.jpg" alt="Une Année album cover"/>
+        <img className="my-slides" src="http://st.cdjapan.co.jp/pictures/l/16/32/NEOIMP-8097.jpg" alt="Secret Garden album cover"/>
+        <img className="my-slides" src="https://upload.wikimedia.org/wikipedia/en/e/e9/PinkBlossom.jpg" alt="Pink Blossom album cover"/>
+        <img className="my-slides" src="http://www.allkpop.com/upload/2014/11/af_org/A-Pink_1417009986_af_org.jpg" alt="Pink Luv of Apink album cover"/>
+        <img className="my-slides" src="http://www.allkpop.com/upload/2015/07/af_org/A-Pink_1437252372_af_org.jpg" alt="Pink Memory album cover"/>
+        <img className="my-slides" src="https://upload.wikimedia.org/wikipedia/en/1/1e/Pink_Revolution_Cover.png" alt="Pink Revolution album cover"/>
+      </div>
+    );
+  }
+}
--- frontend/tribute.jsx
+++ frontend/tribute.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slide from './slide';
 
 export default class Tribute extends React.Component {
   render() {
@@ -12,7 +13,7 @@ export default class Tribute extends React.Component {
           <div className="jumbotron">
             <div className="row">
               <div className="col-xs-12 col-md-12">
-                <h3>You can find more information about this amazing group in their <a href="https://en.wikipedia.org/wiki/Apink" target="_blank">Wikipedia entry</a>.</h3>
+                <Slide />
               </div>
             </div>
           </div>
